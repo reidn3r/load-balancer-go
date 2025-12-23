8ae7718b63a2bafc84681ec94df9eca076f4f48f
reidn3r <rdn.adn00@gmail.com>
2025-12-23 12:00:46 -0300

    feat: add. funcao p/ criacao de backend

--- backend/backend.go
+++ backend/backend.go
@@ -1,8 +1,27 @@
 package backend
 
-import "net/http/httputil"
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+)
 
 type Backend struct {
-	Url   string
-	Proxy httputil.ReverseProxy
+	Url   *url.URL
+	Proxy *httputil.ReverseProxy
+}
+
+func CreateBackend(addr string) *Backend {
+	urlPtr, err := url.Parse(addr)
+
+	if err != nil {
+		msg := fmt.Sprintf("Url: %s", urlPtr.String())
+		fmt.Printf("Erro ao criar novo backend.")
+		fmt.Errorf(msg)
+	}
+
+	return &Backend{
+		Url:   urlPtr,
+		Proxy: httputil.NewSingleHostReverseProxy(urlPtr),
+	}
 }
